7bf10e28ba1360a44e7fc0bb248eb724eb6d445d
UladH <uladzislau.holub@gmail.com>
2023-05-23 17:11:25 +0400

    Added documentation

--- projects/acwrapper/src/lib/examples/custom-wrapper-based-default/custom-wrapper-based-default.module.ts
+++ projects/acwrapper/src/lib/examples/custom-wrapper-based-default/custom-wrapper-based-default.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomWrapperBasedDefaultComponent } from './custom-wrapper-based-default.component';
+import { WrapperComponent } from 'acwrapper'; //here
 
 
 
@@ -9,7 +10,8 @@ import { CustomWrapperBasedDefaultComponent } from './custom-wrapper-based-defau
     CustomWrapperBasedDefaultComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    WrapperComponent //add this line
   ]
 })
 export class CustomWrapperBasedDefaultModule { }
